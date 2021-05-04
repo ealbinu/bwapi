f21bb75690cd8783d88997fbe616f8baf2dc279e
Albin Rdz <albinrdz@MBPro.local>
2021-05-03 19:53:51 -0500

    update

--- dbprogramas.py
+++ dbprogramas.py
@@ -6,15 +6,27 @@ programas = [
         "front": "#fff"
     },
     {
+        "name": "+Kota Loyalty",
+        "id": "maskota",
+        "bg": "#82DDF6",
+        "front": "#602687"
+    },
+    {
+        "name": "Georgie Boy Club",
+        "id": "georgieboy",
+        "bg": "#ffffff",
+        "front": "#004A9C"
+    },
+    {
         "name": "Club Modelorama",
         "id": "clubmodelorama",
         "bg": "#1A2837",
         "front": "#FFBB00"
     },
     {
-        "name": "+Kota Loyalty",
-        "id": "maskota",
-        "bg": "#82DDF6",
-        "front": "#602687"
+        "name": "Página 1",
+        "id": "pagina1",
+        "bg": "#9358A9",
+        "front": "#ffffff"
     },
 ]
