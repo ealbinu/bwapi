bbb02f00b0d4a00d09e831869a3cee1f9b73ff8f
Albin Rdz <albinrdz@MBPro.local>
2021-05-21 11:05:41 -0500

    update db

--- dbpromociones.py
+++ dbpromociones.py
@@ -1,5 +1,27 @@
 usuarioprogramas = [
     {
         "url": "https://bwapiblue.herokuapp.com/static/promo1.jpg",
+        "Title": "Promocion 1",
+        "Description": "Compra ahora y llévate el doble de puntos.",
+    },
+    {
+        "url": "https://bwapiblue.herokuapp.com/static/promo1.jpg",
+        "Title": "Promocion 1",
+        "Description": "Compra ahora y llévate el doble de puntos.",
+    },
+    {
+        "url": "https://bwapiblue.herokuapp.com/static/promo1.jpg",
+        "Title": "Promocion 1",
+        "Description": "Compra ahora y llévate el doble de puntos.",
+    },
+    {
+        "url": "https://bwapiblue.herokuapp.com/static/promo1.jpg",
+        "Title": "Promocion 1",
+        "Description": "Compra ahora y llévate el doble de puntos.",
+    },
+    {
+        "url": "https://bwapiblue.herokuapp.com/static/promo1.jpg",
+        "Title": "Promocion 1",
+        "Description": "Compra ahora y llévate el doble de puntos.",
     },
 ]
